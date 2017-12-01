de9593ee9d9d3575621862ed6b225055f3bdae55
MalhotR1 <rajatmalhotra26@gmail.com>
2017-12-01 13:07:57 +0530

    TPL Parallel.For demo.
    Calculating no. of files and total file size in a directory using foreach loop and Parallel.For loop and comparing results.
    Threadsafe Interlocked.Add method is used in Parallel.For loop.

--- Multithreading/Program.cs
+++ Multithreading/Program.cs
@@ -7,6 +7,15 @@ using System.Threading.Tasks;
 namespace Multithreading {
     class Program {
         static void Main(string[] args) {
+            string path = @"C:\Users\malhotr1\Pictures\New_folder";
+            Console.WriteLine(path);
+            Object res = TPLDemo.ParallelCalculateFileSize(path);
+            Console.WriteLine("ParallelCalulate:{0}", res);
+
+            res = TPLDemo.CalculateFileSize(path);
+            Console.WriteLine("Normal Calulate:{0}", res);
+
+
         }
     }
 }
---
+++ Multithreading/TPLDemo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithreading {
+    class TPLDemo {
+        enum ReturnCode { ResourceNotFound = -1, Ok};
+        public static Object ParallelCalculateFileSize(string path) {
+            long totalSize = 0;
+            int returnCode = 0;
+            if (!Directory.Exists(path)) {
+                returnCode = (int)ReturnCode.ResourceNotFound;                
+            }
+
+            Stopwatch sw = Stopwatch.StartNew();
+            FileInfo[] files = new DirectoryInfo(path).GetFiles();
+            Parallel.For(0, files.Length, i => {
+                long size = files[i].Length;
+                Interlocked.Add(ref totalSize, size);
+            });
+            sw.Stop();
+
+            returnCode = (int)ReturnCode.Ok;
+
+            return new {
+                ResultCode = returnCode,
+                TimeElapsed = sw.ElapsedTicks,
+                Size = totalSize
+            };
+        }
+
+        public static Object CalculateFileSize(string path) {
+            long totalSize = 0;
+            int returnCode = 0;
+            if (!Directory.Exists(path)) {
+                returnCode = (int)ReturnCode.ResourceNotFound;
+            }
+
+            Stopwatch sw = Stopwatch.StartNew();
+            FileInfo[] files = new DirectoryInfo(path).GetFiles();
+            foreach (var file in files) {
+                totalSize += file.Length;
+            }
+            sw.Stop();
+
+            returnCode = (int)ReturnCode.Ok;
+
+            return new {
+                ResultCode = returnCode,
+                TimeElapsed = sw.ElapsedTicks,
+                Size = totalSize
+            };
+        }
+    }
+}
